fd2f23974fed0c911c8052409095a35a64ab4396
Mariska0207 <febriantimar@gmail.com>
2026-03-02 09:11:41 +0800

    commit lagi

--- POSTTEST_1/soal1.cpp
+++ POSTTEST_1/soal1.cpp
@@ -2,10 +2,10 @@
 using namespace std; 
 
 void nilaimin(int arr[], int n){
-    int fibonacci[8]={1,1,2,3,5,8,13,21};
+    int fibonacci[8]={1,1,2,3,5,8,13,1};
     int min = fibonacci[0];
     int index = 0;
-    for(int i=1; i <= n ; i++){
+    for(int i=1; i < n ; i++){
         if(fibonacci[i] < min){
             min = fibonacci[i];
             index = i;
@@ -13,7 +13,7 @@ void nilaimin(int arr[], int n){
     }
     cout << "nilai minimum yaitu : " << min << endl;
     cout << "dengan indeks ke : ";
-    for(int i = 0; i <= n; i++){
+    for(int i = 0; i < n; i++){
         if(fibonacci[i] == min){
             cout << i << " " ;
         }
@@ -22,13 +22,13 @@ void nilaimin(int arr[], int n){
 }
 
 int main(){
-    int fibonacci[8]={1,1,2,3,5,8,13,21};
+    int fibonacci[8]={1,1,2,3,5,8,13,1};
     cout << "nilai fibonacci : " << endl;
     for(int i=0; i<8; i++){
         cout << fibonacci[i] << " " ;
     }
     cout << "\n================================================";
     cout << endl;
-    nilaimin(fibonacci, 7);
+    nilaimin(fibonacci, 8);
     
 }
