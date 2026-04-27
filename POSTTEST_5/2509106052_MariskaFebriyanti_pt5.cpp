6245be5da376074cb56ac79ef8c8521bdd287aa6
Mariska0207 <febriantimar@gmail.com>
2026-04-27 18:19:30 +0800

    pt5selese

--- POSTTEST_5/2509106052_MariskaFebriyanti_pt5.cpp
+++ POSTTEST_5/2509106052_MariskaFebriyanti_pt5.cpp
@@ -2,13 +2,11 @@
 #include <string>
 using namespace std;
 
-// Struktur Node untuk Circular Linked List Toko Sembako
 struct BarangNode {
     string namaBarang;
     int stok;
     BarangNode* next;
 
-    // Constructor
     BarangNode(string nama, int jumlah) {
         namaBarang = nama;
         stok = jumlah;
@@ -16,12 +14,10 @@ struct BarangNode {
     }
 };
 
-// Fungsi insert di akhir Circular Linked List untuk membangun data
 BarangNode* tambahBarang(BarangNode* head, string nama, int jumlah) {
     BarangNode* newNode = new BarangNode(nama, jumlah);
     if (head == nullptr) {
-        newNode->next = newNode; // Menunjuk ke dirinya sendiri (Sirkuler)
-        return newNode;
+        newNode->next = newNode; 
     }
     
     BarangNode* temp = head;
@@ -56,12 +52,5 @@ int main() {
 
     cout << "Daftar Stok Sembako:" << endl;
     tampilkanStokSembako(head); 
-    /* Harusnya output:
-    - Beras: 50
-    - Minyak Goreng: 30
-    - Gula Pasir: 20
-    - Tepung Terigu: 15
-    */
-    
     return 0;
 }
